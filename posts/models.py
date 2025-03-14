6362132fc1f565152283cda9f4604d74d70289b8
Vonda <lavondedunigan05@gmail.com>
2025-03-13 22:43:33 -0500

    fixed error

---
+++ posts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):                                   # inheritance
+    title = models.CharField(max_length=120)                # composition
+    subtitle = models.CharField(max_length=255)             # composition
+    body = models.TextField()                               # composition 
+    created_on = models.DateTimeField(auto_now_add=True)    # composition
+  
+    def __str__(self):
+        return self.title
